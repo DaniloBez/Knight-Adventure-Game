aa64104287d3661a5dd2748349973f46fb8c13e5
DaniloBez <Danylo.bezukh@gmail.com>
2025-05-23 19:00:44 +0300

    Add javadoc

--- core/src/main/java/Assembly/Enjoyers/Character.java
+++ core/src/main/java/Assembly/Enjoyers/Character.java
@@ -47,7 +47,9 @@ public class Character {
     // --- UI ---
     private final ShapeRenderer shapeRenderer = new ShapeRenderer();
 
-
+    /**
+     * Конструктор персонажа, ініціалізує текстуру, спрайт та хитбокс.
+     */
     Character(){
         texture = new Texture("player\\knight_green_idle_01.png");
         sprite = new Sprite(texture);
@@ -56,32 +58,89 @@ public class Character {
         hitBox = new Rectangle(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight());
     }
 
+    /**
+     * Основна функція оновлення руху та взаємодії з рівнем.
+     * @param bounds список прямокутників колізій
+     */
     public void move(List<Rectangle> bounds) {
         float delta = graphics.getDeltaTime();
 
-        float moveX = 0;
-        if (input.isKeyPressed(Keys.A)) moveX -= moveSpeed * delta;
-        if (input.isKeyPressed(Keys.D)) moveX += moveSpeed * delta;
-
-        velocityY += gravity * delta;
-
+        float moveX = handleHorizontalInput(delta);
+        applyGravity(delta);
         dash();
 
         updateHitBox();
         onGround = checkFeetTouching(bounds);
-        if (onGround) {
-            dashCount = 1;
-            stamina = 100f;
-        }
+        if (onGround) resetDashAndStamina();
 
         touchingWall = checkWallTouching(bounds);
+        handleJump(bounds);
+        handleWallInteraction(bounds, delta);
 
+        applyVerticalMovement(bounds, delta);
+        applyHorizontalMovement(moveX, bounds, delta);
+    }
 
-        if (onGround && input.isKeyJustPressed(Keys.SPACE)) {
-            velocityY = jumpForce;
+    /**
+     * Застосовує горизонтальний рух гравця та перевіряє зіткнення.
+     * @param moveX Рух по осі OX
+     * @param bounds Список хіт боксів у рівні
+     * @param delta Проміжок часу між поточним та останнім кадром у секундах.
+     */
+    private void applyHorizontalMovement(float moveX, List<Rectangle> bounds, float delta) {
+        if (Math.abs(velocityX) > 600) {
+            sprite.translateX((velocityX + dashXVelocity) * delta);
+            velocityX *= 0.95f;
+            if (Math.abs(velocityX) < 10f) velocityX = 0;
         }
+        else
+            sprite.translateX(moveX + dashXVelocity * delta);
 
+        updateHitBox();
+        for (Rectangle bound : bounds) {
+            if (hitBox.overlaps(bound)) {
+                if (moveX > 0)
+                    sprite.setX(bound.x - hitBox.width);
+                else if (moveX < 0)
+                    sprite.setX(bound.x + bound.width);
 
+                dashXVelocity = 0;
+                updateHitBox();
+                break;
+            }
+        }
+    }
+
+    /**
+     * Застосовує вертикальний рух гравця та перевіряє зіткнення.
+     * @param bounds Список хіт боксів у рівні
+     * @param delta Проміжок часу між поточним та останнім кадром у секундах.
+     */
+    private void applyVerticalMovement(List<Rectangle> bounds, float delta) {
+        sprite.translateY((velocityY + dashYVelocity) * delta);
+        updateHitBox();
+        for (Rectangle bound : bounds) {
+            if (hitBox.overlaps(bound)) {
+                if (velocityY > 0) {
+                    sprite.setY(bound.y - hitBox.height);
+                } else if (velocityY < 0) {
+                    sprite.setY(bound.y + bound.height);
+                    onGround = true;
+                }
+                velocityY = 0;
+                dashYVelocity = 0;
+                updateHitBox();
+                break;
+            }
+        }
+    }
+
+    /**
+     * Обробка поведінки персонажа при взаємодії зі стіною: стаміна, ковзання, лазання.
+     * @param bounds Список хіт боксів у рівні
+     * @param delta Проміжок часу між поточним та останнім кадром у секундах.
+     */
+    private void handleWallInteraction(List<Rectangle> bounds, float delta) {
         if (touchingWall && !onGround) {
             if (input.isButtonPressed(Input.Buttons.RIGHT) && stamina > 0) {
 
@@ -131,48 +190,48 @@ public class Character {
                 }
             }
         }
+    }
 
-
-        sprite.translateY((velocityY + dashYVelocity) * delta);
-        updateHitBox();
-        for (Rectangle bound : bounds) {
-            if (hitBox.overlaps(bound)) {
-                if (velocityY > 0) {
-                    sprite.setY(bound.y - hitBox.height);
-                } else if (velocityY < 0) {
-                    sprite.setY(bound.y + bound.height);
-                    onGround = true;
-                }
-                velocityY = 0;
-                dashYVelocity = 0;
-                updateHitBox();
-                break;
-            }
-        }
-
-        if (Math.abs(velocityX) > 600) {
-            sprite.translateX((velocityX + dashXVelocity) * delta);
-            velocityX *= 0.95f;
-            if (Math.abs(velocityX) < 10f) velocityX = 0;
-        }
-        else
-            sprite.translateX(moveX + dashXVelocity * delta);
-
-        updateHitBox();
-        for (Rectangle bound : bounds) {
-            if (hitBox.overlaps(bound)) {
-                if (moveX > 0)
-                    sprite.setX(bound.x - hitBox.width);
-                else if (moveX < 0)
-                    sprite.setX(bound.x + bound.width);
-
-                dashXVelocity = 0;
-                updateHitBox();
-                break;
-            }
+    /**
+     * Стрибок з землі.
+     * @param bounds Список хіт боксів у рівні
+     */
+    private void handleJump(List<Rectangle> bounds) {
+        if (onGround && input.isKeyJustPressed(Keys.SPACE)) {
+            velocityY = jumpForce;
         }
     }
 
+    /**
+     * Скидає лічильники деша та стаміни, коли персонаж на землі.
+     */
+    private void resetDashAndStamina() {
+        dashCount = 1;
+        stamina = 100f;
+    }
+    /**
+     * Обробляє вхідні команди AD для руху вліво/вправо.
+     * @param delta Проміжок часу між поточним та останнім кадром у секундах.
+     * @return зсув по осі OX
+     */
+    private float handleHorizontalInput(float delta) {
+        float moveX = 0;
+        if (input.isKeyPressed(Keys.A)) moveX -= moveSpeed * delta;
+        if (input.isKeyPressed(Keys.D)) moveX += moveSpeed * delta;
+        return moveX;
+    }
+
+    /**
+     * Застосовує гравітацію до вертикальної швидкості.
+     * @param delta Проміжок часу між поточним та останнім кадром у секундах.
+     */
+    private void applyGravity(float delta) {
+        velocityY += gravity * delta;
+    }
+
+    /**
+     * Обробляє деш у різні сторони (LMB), з лімітом на кількість.
+     */
     private void dash(){
         dashXVelocity *= 0.98f;
         dashYVelocity *= 0.98f;
@@ -196,10 +255,18 @@ public class Character {
         }
     }
 
+    /**
+     * Оновлює координати прямокутника hitBox за позицією спрайта.
+     */
     private void updateHitBox() {
         hitBox.set(sprite.getX(), sprite.getY(), sprite.getWidth() - 50f, sprite.getHeight());
     }
 
+    /**
+     * Перевіряє, чи персонаж стоїть на землі.
+     * @param bounds Список хіт боксів у рівні
+     * @return чи персонаж стоїть на землі
+     */
     private boolean checkFeetTouching(List<Rectangle> bounds) {
         Rectangle feet = new Rectangle(hitBox.x + hitBox.width / 8, hitBox.y - 1, hitBox.width / 4 * 3, 2);
         for (Rectangle bound : bounds)
@@ -209,6 +276,11 @@ public class Character {
         return false;
     }
 
+    /**
+     * Перевіряє контакт правої сторони з перешкодою.
+     * @param bounds Список хіт боксів у рівні
+     * @return чи персонаж дотикаєтеся справа
+     */
     private boolean checkRightTouching(List<Rectangle> bounds) {
         Rectangle right = new Rectangle( hitBox.x + hitBox.width - 2, hitBox.y - hitBox.height / 8, 4, hitBox.height / 4 * 3);
         for (Rectangle bound : bounds)
@@ -218,6 +290,11 @@ public class Character {
         return false;
     }
 
+    /**
+     * Перевіряє контакт лівої сторони з перешкодою.
+     * @param bounds Список хіт боксів у рівні
+     * @return чи персонаж дотикаєтеся зліва
+     */
     private boolean checkLeftTouching(List<Rectangle> bounds) {
         Rectangle left = new Rectangle( hitBox.x - 2, hitBox.y - hitBox.height / 8, 4, hitBox.height / 4 * 3);
         for (Rectangle bound : bounds)
@@ -227,6 +304,11 @@ public class Character {
         return false;
     }
 
+    /**
+     * Перевіряє, чи персонаж знаходиться поруч зі стіною (ліворуч або праворуч) (для ковзання).
+     * @param bounds Список хіт боксів у рівні
+     * @return чи персонаж дотикаєтеся справа або зліва
+     */
     private boolean checkWallTouching(List<Rectangle> bounds) {
         Rectangle testX1 = new Rectangle(hitBox);
         Rectangle testX2 = new Rectangle(hitBox);
@@ -239,6 +321,10 @@ public class Character {
         return false;
     }
 
+    /**
+     * Малює HUD-полоску стаміни у верхній частині екрана.
+     * @param camera камера для HUD
+     */
     public void drawStaminaBar(OrthographicCamera camera) {
         shapeRenderer.setProjectionMatrix(camera.combined);
 
